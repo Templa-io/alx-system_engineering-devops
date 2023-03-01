837b11ec93bffde63a9c79f03c500257bd7f7918
Templa-io <humphreywritter@gmail.com>
2023-03-01 03:44:17 -0800

    update files

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,8 @@
+input = ARGV[0]
+
+if input =~ /School/
+  puts "The input '#{input}' matches the regular expression /School/."
+else
+  puts "The input '#{input}' does not match the regular expression /School/."
+end
+
